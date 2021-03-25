078b1d6021d9920e547e9d0c8c68d74ad7dff267
David Mayo <explosivepiedm@gmail.com>
2021-03-26 00:11:36 +1300

    added some more sound commands. added 8 ball command

--- bot.js
+++ bot.js
@@ -40,11 +40,64 @@ client.on('message', async message => {
     // console.log(message.channel.guild.members.cache.get(userID).voice.serverMute = true);
     // console.log(client.users);
 
-    if (content === '-test') {
+    if (content.split(' ')[0] === '-ask') {
+        const options = [
+            'As I see it, yes.',
+            'Ask again later.',
+            'Better not tell you now.',
+            'Cannot predict now.',
+            'Concentrate and ask again.',
+            'Don’t count on it.',
+            'It is certain.',
+            'It is decidedly so.',
+            'Most likely.',
+            'My reply is no.',
+            'My sources say no.',
+            'Outlook not so good.',
+            'Outlook good.',
+            'Reply hazy, try again.',
+            'Signs point to yes.',
+            'Very doubtful.',
+            'Without a doubt.',
+            'Yes.',
+            'Yes – definitely.',
+            'You may rely on it.'
+        ];
+        const index = Math.floor(Math.random() * options.length);
+        helpers.sendEmbeddedMessage(channelID, {description: options[index]});
+    }
+
+    if (content === '-jeff') {
         const voiceChannelID = message.member.voice.channel;
         if (voiceChannelID) {
             const connection = await message.member.voice.channel.join();
-            const dispatcher = connection.play('jeff.mp3', {volume: 1});
+            const dispatcher = connection.play('assets/mp3/jeff.mp3', {volume: 1});
+            dispatcher.on('finish', () => {
+                dispatcher.destroy();
+                message.member.voice.channel.leave();
+            });
+        }
+        return;
+    }
+
+    if (content === '-racism') {
+        const voiceChannelID = message.member.voice.channel;
+        if (voiceChannelID) {
+            const connection = await message.member.voice.channel.join();
+            const dispatcher = connection.play('assets/mp3/racism.mp3', {volume: 1});
+            dispatcher.on('finish', () => {
+                dispatcher.destroy();
+                message.member.voice.channel.leave();
+            });
+        }
+        return;
+    }
+
+    if (content === '-11') {
+        const voiceChannelID = message.member.voice.channel;
+        if (voiceChannelID) {
+            const connection = await message.member.voice.channel.join();
+            const dispatcher = connection.play('assets/mp3/wololo.mp3', {volume: 1});
             dispatcher.on('finish', () => {
                 dispatcher.destroy();
                 message.member.voice.channel.leave();
